ce35cbf4177d30e71d16ffbe343117d62562eaf5
DULANA CHATHURMA <dulanachathurma99@gmail.com>
2026-07-04 15:34:11 +0530

    Modify image paths in lagnaData
    Updated image paths for lagnaData entries to include 'lagna_' prefix.

--- src/data/lagnaData.js
+++ src/data/lagnaData.js
@@ -1,14 +1,14 @@
 export const lagnaData = [
-  { name: 'මේෂ', image: '/images/mesha.jpg', description: 'මේෂ ලග්නය උත්සාහවන්ත බවත්, නිර්භීත බවත් සංකේතවත් කරන අඟහරු ග්‍රහයා අධිපති ලග්නයයි.' },
-  { name: 'වෘෂභ', image: '/images/vrushaba.jpg', description: 'වෘෂභ ලග්නය ඉවසීම, ස්ථාවරත්වය සහ කලාත්මක බව මුල් කරගත් සිකුරු අධිපති ලග්නයකි.' },
-  { name: 'මිථුන', image: '/images/mithuna.jpg', description: 'මිථුන ලග්නය බුද්ධිමත් බවත්, සමාජශීලී බවත් විදහා දක්වන බුධ ග්‍රහයා අධිපති ලග්නයයි.' },
-  { name: 'කටක', image: '/images/kataka.jpg', description: 'කටක ලග්නය හැඟීම්බර සහ සෙනෙහසින් පිරි සඳු ග්‍රහයා අධිපති ලග්නයකි.' },
-  { name: 'සිංහ', image: '/images/sinha.jpg', description: 'සිංහ ලග්නය ආත්ම විශ්වාසය සහ නායකත්ව ගුණාංගවලට උරුමකම් කියන රවි අධිපති ලග්නයයි.' },
-  { name: 'කන්‍යා', image: '/images/kanya.jpg', description: 'කන්‍යා ලග්නය ඉතා ක්‍රමානුකූල සහ විශ්ලේෂණාත්මක බුධ ග්‍රහයා අධිපති ලග්නයකි.' },
-  { name: 'තුලා', image: '/images/thula.jpg', description: 'තුලා ලග්නය සමබරතාවය සහ යුක්තිගරුක බව අගය කරන සිකුරු අධිපති ලග්නයයි.' },
-  { name: 'වෘශ්චික', image: '/images/vrushchika.jpg', description: 'වෘශ්චික ලග්නය තීක්ෂණ බුද්ධිය සහ අධිෂ්ඨානය සහිත අඟහරු අධිපති ලග්නයකි.' },
-  { name: 'ධනු', image: '/images/danu.jpg', description: 'ධනු ලග්නය නිදහස සහ සුභවාදී බව පිරුණු ගුරු ග්‍රහයා අධිපති ලග්නයකි.' },
-  { name: 'මකර', image: '/images/makara.jpg', description: 'මකර ලග්නය කැපවීම සහ අරමුණු ජයගැනීමේ හැකියාව සහිත සෙනසුරු අධිපති ලග්නයයි.' },
-  { name: 'කුම්භ', image: '/images/kumbha.jpg', description: 'කුම්භ ලග්නය නවීන සිතුවිලි සහ මානුෂීය ගුණාංග සහිත සෙනසුරු අධිපති ලග්නයකි.' },
-  { name: 'මීන', image: '/images/meena.jpg', description: 'මීන ලග්නය නිර්මාණශීලී සහ කරුණාවන්ත ගුරු ග්‍රහයා අධිපති ලග්නයයි.' }
+  { name: 'මේෂ', image: '/images/lagna_mesha.jpg', description: 'මේෂ ලග්නය උත්සාහවන්ත බවත්, නිර්භීත බවත් සංකේතවත් කරන අඟහරු ග්‍රහයා අධිපති ලග්නයයි.' },
+  { name: 'වෘෂභ', image: '/images/lagna_vrishaba.jpg', description: 'වෘෂභ ලග්නය ඉවසීම, ස්ථාවරත්වය සහ කලාත්මක බව මුල් කරගත් සිකුරු අධිපති ලග්නයකි.' },
+  { name: 'මිථුන', image: '/images/lagna_mithuna.jpg', description: 'මිථුන ලග්නය බුද්ධිමත් බවත්, සමාජශීලී බවත් විදහා දක්වන බුධ ග්‍රහයා අධිපති ලග්නයයි.' },
+  { name: 'කටක', image: '/images/lagna_kataka.jpg', description: 'කටක ලග්නය හැඟීම්බර සහ සෙනෙහසින් පිරි සඳු ග්‍රහයා අධිපති ලග්නයකි.' },
+  { name: 'සිංහ', image: '/images/lagna_sinha.jpg', description: 'සිංහ ලග්නය ආත්ම විශ්වාසය සහ නායකත්ව ගුණාංගවලට උරුමකම් කියන රවි අධිපති ලග්නයයි.' },
+  { name: 'කන්‍යා', image: '/images/lagna_kanya.jpg', description: 'කන්‍යා ලග්නය ඉතා ක්‍රමානුකූල සහ විශ්ලේෂණාත්මක බුධ ග්‍රහයා අධිපති ලග්නයකි.' },
+  { name: 'තුලා', image: '/images/lagna_tula.jpg', description: 'තුලා ලග්නය සමබරතාවය සහ යුක්තිගරුක බව අගය කරන සිකුරු අධිපති ලග්නයයි.' },
+  { name: 'වෘශ්චික', image: '/images/lagna_vrischika.jpg', description: 'වෘශ්චික ලග්නය තීක්ෂණ බුද්ධිය සහ අධිෂ්ඨානය සහිත අඟහරු අධිපති ලග්නයකි.' },
+  { name: 'ධනු', image: '/images/lagna_dhanu.jpg', description: 'ධනු ලග්නය නිදහස සහ සුභවාදී බව පිරුණු ගුරු ග්‍රහයා අධිපති ලග්නයකි.' },
+  { name: 'මකර', image: '/images/lagna_makara.jpg', description: 'මකර ලග්නය කැපවීම සහ අරමුණු ජයගැනීමේ හැකියාව සහිත සෙනසුරු අධිපති ලග්නයයි.' },
+  { name: 'කුම්භ', image: '/images/lagna_kumbha.jpg', description: 'කුම්භ ලග්නය නවීන සිතුවිලි සහ මානුෂීය ගුණාංග සහිත සෙනසුරු අධිපති ලග්නයකි.' },
+  { name: 'මීන', image: '/images/lagna_meena.jpg', description: 'මීන ලග්නය නිර්මාණශීලී සහ කරුණාවන්ත ගුරු ග්‍රහයා අධිපති ලග්නයයි.' }
 ];
